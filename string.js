44a5426d88a03cf560f8acfd0acddcafdfbc6746
k-vasanth09072002 <vasanthvasath74@gmail.com>
2024-03-26 19:01:18 +0530

    string-methods

---
+++ string.js
@@ -0,0 +1,68 @@
+console.log("lenght")
+let test="vasanth"
+console.log(test.length);
+
+///charAt()
+
+
+console.log("charAt()");
+var cha="vasanth"
+console.log(cha.charAt(2));
+
+
+console.log("charCodeAt()");
+var chars="hello world";
+console.log(chars.charCodeAt(4));
+
+
+console.log("at");
+
+var names="vasanth"
+console.log(names.at(0));
+
+// console.log(slice);
+// var fuird="apply,orange"
+// console.log(fuird.slice(4));
+
+console.log("uppercase");
+var upp="vasanth";
+console.log(upp.toUpperCase());
+
+console.log("toLowerCase()");
+var low="VASANTH"
+console.log(low.toLowerCase());
+
+
+console.log(" concat()");
+
+var con1="hello";
+var con2="world";
+console.log(con1.concat(con2));
+
+
+console.log("trim");
+var tr="vasanth"
+var tri=tr.trim()
+console.log(tri);
+
+console.log("trimStart()");
+var trims="      vasanth"
+var trims1=trims.trimStart()
+console.log(trims1);
+
+
+console.log("padStart() Method");
+let text3 = "5";
+text5 = text3.padStart(2,"1");
+console.log(text5);
+
+console.log("padEnd() Method");
+let text6= "5";
+text7= text6.padEnd(2,"1");
+console.log(text7);
+
+console.log("repeat()");
+
+var rep="hello world!";
+var repe=rep.repeat(3);
+console.log(repe);
